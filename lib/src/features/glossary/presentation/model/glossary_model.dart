c6f3ee6b4c8dce3806fa7ab69f6df8b7579fadaf
karen98bdiw <ghazaryan.deb@mail.ru>
2021-11-05 11:26:30 +0400

    gloosary ui fixes,hovering griv view items

--- lib/src/features/glossary/presentation/model/glossary_model.dart
+++ lib/src/features/glossary/presentation/model/glossary_model.dart
@@ -1,6 +1,11 @@
 class GlossaryModel {
   final String title;
   final String text;
+  final int index;
 
-  GlossaryModel({required this.title, required this.text});
+  GlossaryModel({
+    required this.title,
+    required this.text,
+    required this.index,
+  });
 }
