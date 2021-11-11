cd5f01e0c86a5b2330b656d528348547d7d424a5
karen98bdiw <ghazaryan.deb@mail.ru>
2021-11-11 12:52:57 +0400

    added map quiz ui

---
+++ lib/src/features/quiz/presentation/question_widgets/drag_drop_widgets/map_image_item.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:history_of_adventures/src/core/utils/styles.dart';
+import 'package:history_of_adventures/src/features/quiz/data/map_quiz_model.dart';
+
+class MapImageItem extends StatelessWidget {
+  final MapQuizItemModel model;
+  const MapImageItem({
+    Key? key,
+    required this.model,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          model.title,
+          style: Theme.of(context).textTheme.overline!.copyWith(
+                fontSize: HW.getHeight(18, context),
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+        SizedBox(
+          width: HW.getWidth(16, context),
+        ),
+        Expanded(
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset(
+              model.assets,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
