1c1f9629fe8adabac18bfae8ec54bebce1db80fc
tsovinar <tsov.vardanyan2000@gmail.com>
2022-03-11 10:44:12 +0400

    Changed loading view for pages

--- lib/src/core/widgets/loading_widget.dart
+++ lib/src/core/widgets/loading_widget.dart
@@ -22,75 +22,50 @@ class LoadingWidget extends StatelessWidget {
       body: Container(
         color: color ?? AppColors.black100.withOpacity(0.5),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Stack(
-                children: [
-                  ImageFill(
-                      boxHeight: HW.getWidth(200, context),
-                      boxWidth: HW.getWidth(200, context),
-                      loadingCount: int.parse(loadingCound!)),
-
-                  // Image.asset(
-                  //   AssetsPath.loadingVirus1,
-                  //   width: HW.getWidth(200, context),
-                  //   height: HW.getWidth(200, context),
-                  // ),
-
-                  // Visibility(
-                  //   visible: int.parse(loadingCound!) >= 50,
-                  //   child: Image.asset(
-                  //     AssetsPath.loadingVirus2,
-                  //     width: HW.getWidth(200, context),
-                  //     height: HW.getWidth(200, context),
-                  //   ),
-                  // ),
-                  // Visibility(
-                  //   visible: int.parse(loadingCound!) == 99,
-                  //   child: Image.asset(
-                  //     AssetsPath.loadingVirus3,
-                  //     width: HW.getWidth(200, context),
-                  //     height: HW.getWidth(200, context),
-                  //   ),
-                  // ),
-                ],
-              ),
-              Container(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: Row(
+          child: loadingCound != null
+              ? Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    if (int.parse(loadingCound!) != 99)
-                      const Text(
-                        "LOADING  ",
-                        style: TextStyle(fontSize: 30, color: Colors.white),
+                    ImageFill(
+                        boxHeight: HW.getWidth(200, context),
+                        boxWidth: HW.getWidth(200, context),
+                        loadingCount: int.parse(loadingCound!)),
+                    Container(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          if (int.parse(loadingCound!) != 99)
+                            const Text(
+                              "LOADING  ",
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white),
+                            ),
+                          if (int.parse(loadingCound!) == 99)
+                            const Text(
+                              "  Click anywhere to start",
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.orange),
+                            ),
+                          if (loadingCound != null && loadingCound != "99")
+                            Text(
+                              '$loadingCound%',
+                              style: const TextStyle(
+                                  fontSize: 30, color: Colors.white),
+                            )
+                        ],
                       ),
-                    if (int.parse(loadingCound!) == 99)
-                      const Text(
-                        "  Click anywhere to start",
-                        style: TextStyle(fontSize: 30, color: Colors.orange),
-                      ),
-
-                    // PulsingWidget(
-                    //   duration: Times.medium,
-                    //   tween: Tween(end: 0.25, begin: 1.5),
-                    //   child: const Text(
-                    //     "LOADING",
-                    //     style: TextStyle(fontSize: 30, color: Colors.white),
-                    //   ),
-                    // ),
-                    if (loadingCound != null && loadingCound != "99")
-                      Text(
-                        '$loadingCound%',
-                        style:
-                            const TextStyle(fontSize: 30, color: Colors.white),
-                      )
+                    ),
                   ],
+                )
+              : PulsingWidget(
+                  duration: Times.medium,
+                  tween: Tween(end: 0.25, begin: 1.5),
+                  child: const Text(
+                    "Loading...",
+                    style: TextStyle(fontSize: 30, color: Colors.white),
+                  ),
                 ),
-              ),
-            ],
-          ),
         ),
       ),
     );
